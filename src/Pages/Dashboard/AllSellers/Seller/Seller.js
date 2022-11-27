4434a7f2851d33f737e911553ca521ddf65bb1cb
Kamronnahar-Monisha <kamronnaharmonisha12@gmail.com>
2022-11-27 20:24:04 +0600

    All seller page updated

--- src/Pages/Dashboard/AllSellers/Seller/Seller.js
+++ src/Pages/Dashboard/AllSellers/Seller/Seller.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../../../Context/AuthProvider';
+import { useLogOutTheUser } from '../../../../hooks/useLogOutTheUser';
 import './Seller.css';
 
 const Seller = ({ seller, refetch }) => {
     const { name, email, _id, verified } = seller;
+    const { user } = useContext(AuthContext);
+    const logOutUser = useLogOutTheUser();
+
+    const handleUserDelete = () => {
+        const proceed = window.confirm("Are you sure you want to delete the User?");
+        if (!proceed) {
+            return;
+        }
+        fetch(`http://localhost:5000/users/${_id}?email=${user.email}`, {
+            method: "DELETE",
+            headers: {
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => {
+                if ((res.status === 403) || (res.status === 401)) {
+                    toast.error("Unauthorized Access.");
+                    logOutUser();
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success('User Deleted Successfully.');
+                    refetch();
+                }
+            });
+    }
+
+    const handleUserVerify = () => {
+
+        fetch(`http://localhost:5000/users/${_id}?email=${user.email}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({ verified: true })
+        })
+            .then(res => {
+                if ((res.status === 403) || (res.status === 401)) {
+                    toast.error("Unauthorized Access.");
+                    logOutUser();
+                }
+                return res.json()
+            })
+            .then(data => {
+                if (data?.acknowledged) {
+                    toast.success('User Verified Successfully.');
+                    refetch();
+                }
+            });
+    }
+
     return (
         <tr className='text-muted'>
             <td>{name}</td>
@@ -12,11 +69,11 @@ const Seller = ({ seller, refetch }) => {
                     verified ?
                         <button className='btn-sm theme-button-outline' disabled>verified</button>
                         :
-                        <button className='btn-sm theme-button-outline'>verify</button>
+                        <button className='btn-sm theme-button-outline' onClick={handleUserVerify}>verify</button>
                 }
             </td>
             <td>
-                <button className='btn-sm theme-button'>Delete</button>
+                <button className='btn-sm theme-button' onClick={handleUserDelete}>Delete</button>
             </td>
         </tr>
     );
